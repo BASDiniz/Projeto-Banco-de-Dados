60a130644847d67619d292131826f5b31deb5686
ErikJhonatta <erikjhonatta@hotmail.com>
2019-06-02 17:39:14 +0000

    Adicionando Historico de vendas funcional

--- src/gui/historicoVenda/FXMLHistoricoVendaController.java
+++ src/gui/historicoVenda/FXMLHistoricoVendaController.java
@@ -1,5 +1,7 @@
 package gui.historicoVenda;
 
+import fachada.FachadaGerente;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
@@ -8,42 +10,62 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import negocio.entidade.Cliente;
 import negocio.entidade.Funcionario;
 import negocio.entidade.Venda;
+import negocio.entidade.VendaForm;
 
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.ResourceBundle;
 
 
 public class FXMLHistoricoVendaController implements Initializable {
 
     @FXML
-    private TableView<Venda> tabela;
+    private TableView<VendaForm> tabela;
 
     @FXML
-    private TableColumn<Venda, String> codigoVenda;
+    private TableColumn<VendaForm, String> codigoVenda;
 
     @FXML
-    private TableColumn<Funcionario, String> cpfFunc;
+    private TableColumn<VendaForm, String> cpfFunc;
 
     @FXML
-    private TableColumn<Cliente, String> cpfCliente;
+    private TableColumn<VendaForm, String> cpfCliente;
 
     @FXML
-    private TableColumn<Venda,String> dataVenda;
+    private TableColumn<VendaForm,String> dataVenda;
+
+//    @FXML
+//    private TableColumn<Venda, String> carrinhoVenda;
 
     @FXML
-    private TableColumn<Venda, String> carrinhoVenda;
+    private TableColumn<VendaForm, String> parcelado;
 
     @FXML
-    private TableColumn<Venda, String> parcelado;
-
-    @FXML
-    private TableColumn<Venda, String> valor;
+    private TableColumn<VendaForm, String> valor;
 
     @Override
     public void initialize(URL url, ResourceBundle rb){
-        this.codigoVenda.setCellValueFactory(new PropertyValueFactory<Venda, String>("codigo"));
-        this.cpfFunc.setCellValueFactory(new PropertyValueFactory<Funcionario, String>("cpf"));
+        this.codigoVenda.setCellValueFactory(new PropertyValueFactory<VendaForm, String>("cod"));
+        this.cpfFunc.setCellValueFactory(new PropertyValueFactory<VendaForm, String>("cpfFunc"));
+        this.cpfCliente.setCellValueFactory(new PropertyValueFactory<VendaForm, String>("cpfCli"));
+        this.dataVenda.setCellValueFactory(new PropertyValueFactory<VendaForm, String>("data"));
+        this.parcelado.setCellValueFactory(new PropertyValueFactory<VendaForm, String>("parcelado"));
+        this.valor.setCellValueFactory(new PropertyValueFactory<VendaForm, String>("valor"));
 
-        //chamar método pra retornar o array de vendas
+        ArrayList<Venda> lista = FachadaGerente.getInstance().historicoVendas();
+        ArrayList<VendaForm> show = new ArrayList<VendaForm>();
+        for (Venda item:lista) {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            String data = sdf.format(item.getDataDeVenda().getTime());
+//          item.getDataDeVenda()
+            VendaForm temp = new VendaForm(Integer.toString(item.getCodigo()),item.getFuncionario().getCpf(),item.getCliente().getCpf(),data,Boolean.toString(item.getPagamenteParcelado()),Double.toString(item.getValorTotal()));
+            show.add(temp);
+        }
+        this.tabela.setItems(FXCollections.observableArrayList(show));
     }
+
+
+
 }
